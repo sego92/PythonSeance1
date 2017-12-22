e13ddde8e4f68438bfef1fd3308c80ac5d6b1d8d
sego <sego.maris@gmail.com>
2017-12-22 10:29:59 +0100

    modif python file

--- webScraping.py
+++ webScraping.py
@@ -5,7 +5,9 @@ pageWeb = requests.get('https://twitter.com/EmmanuelMacron')
 
 tree = html.fromstring(pageWeb.content)
 
-followerCount = tree.xpath('//*[@id="page-container"]/div[1]/div/div[2]/div/div/div[2]/div/div/ul/li[3]/a/span[1]')
+followerCount = tree.xpath('//*[@id="page-container"]/div[1]/div/div[2]/div/div/div[2]/div/div/ul/li[3]/a/span[3]/text()')
+
+#followerCount = tree.xpath('//@data-count')
 
 print (followerCount)
-
+# print (followerCount[2])
